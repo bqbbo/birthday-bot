f120fd3e1260e0a4d0077daf873322dbb03008b0
Bobby Elmore <bobbye23899@gmail.com>
2025-07-13 14:25:02 -0700

    Fix barreling error

--- src/events/index.js
+++ src/events/index.js
@@ -1,4 +1,3 @@
 export { default as createInteraction } from "./createInteraction.js";
-export { default as handleCommandInteraction } from "./handler/handleCommandInteraction.js";
 export { default as join } from "./join.js";
 export { default as ready } from "./ready.js";
